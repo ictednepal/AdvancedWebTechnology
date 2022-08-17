4a81eea63d69b1b77547a493c5b35e032c2d9412
Vijaya Malla <mallabiju@gmail.com>
2022-08-17 09:29:37 +0545

    PHP control flow

---
+++ Lab6 ControlFlow/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    // $gender = "male";
+    // if ($gender == "male") {
+    //     echo "Hello Mister";
+    // }
+
+    // $num = 56;
+    // if($num >0){
+    //     echo $num." is +ve number";
+    // }
+
+    // $num = -56;
+    // if($num >0){
+    //     echo $num." is +ve number";
+    // }
+    // else{
+    //     echo $num." is -ve number";
+    // }
+
+
+    $per = 99;
+    if($per>=80){
+        echo "Distinction";
+    }
+    else if($per>=60){
+        echo "First";
+    }
+    else if($per>=45){
+        echo "Second";
+    }
+    else if($per>=32){
+        echo "Third";
+    }
+    else{
+        echo "Failed";
+    }
+
+
+
+
+    ?>
+</body>
+
+</html>
